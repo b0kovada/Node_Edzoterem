ff84d6d982a7b664d5fe7dcfe1a7587c7467bfa6
b0kovada <drippy_tess@icloud.com>
2024-10-30 14:45:46 +0100

    Data & Models - kesz

---
+++ models/training.js
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import path from 'path'
+import __dirname from '../util/rootpath.js'
+
+const FILE_PATH = path.join(__dirname, 'data', 'timetable.json')
+
+const getFileConent = () => {
+    let content = []
+    try{
+        content = JSON.parse(fs.readFileSync(FILE_PATH, 'utf8'))
+    }catch(err){
+        console.log(`File reading error: ${err}`)
+    }
+    return content
+}
+
+const setFileConent = (content) => {
+    try{
+        fs.writeFileSync(FILE_PATH, JSON.stringify(content))
+    }catch(err){
+        console.log(`File reading error: ${err}`)
+    }
+}
+
+class Training {
+    constructor(title, time, master){
+        this.title = title,
+        this. time = time,
+        this.master = master
+    }
+
+    save(){
+        const subjects = getFileConent()
+        subjects.push(this)
+        setFileConent(subjects)
+    }
+
+    static getAllSubjects(){
+        return getFileConent()
+    }
+}
+
+export default Training
