cc94359fd96270e756a7eafbd4479d098ad4704b
Benedek Krisztin <b0kriben@vasvari.org>
2024-10-30 11:24:01 +0100

    controllers: timetable.js ver 1.1

---
+++ controllers/timetable.js
@@ -0,0 +1,23 @@
+import Training from '../models/traning.js'
+
+
+export const getAddTraining = (req, res) => {
+    res.render('add-training.ejs', {
+        pageTitle: 'Add Training',
+        path: '/admin/add-training'
+    })
+}
+
+export const postAddTraining = (req, res) => {
+    const training = new Training(req.body.title)
+    training.save()
+    res.redirect('/')
+}
+
+export const getAllTrainings = (req, res) => {
+    res.render('timetable.ejs', {
+        pageTitle: 'Timetable',
+        path: '/',
+        prods: Product.getAllTrainings()
+    })
+}
