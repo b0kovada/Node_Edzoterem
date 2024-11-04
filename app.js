a8d590a6b0e12d4eed00d8a18ce728b06cf27489
b0kovada <drippy_tess@icloud.com>
2024-11-04 20:28:26 +0100

    fixup v1

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ import __dirname from './util/rootpath.js'
 
 const app = express()
 const PORT = 3000
-//const router = express.Router();
 
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -16,8 +15,8 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', timetableRoutes) //!!
-app.use('/admin', adminRoutes) //!!
+app.use('/', timetableRoutes) 
+app.use('/admin', adminRoutes) 
 
 app.use((req, res) => {
     res.status(404).render('404.ejs', {
